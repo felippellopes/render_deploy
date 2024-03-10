376931ef457442158894a644946bc89afee29043
Felipe Lopes <fl0924146@gmail.com>
2024-03-10 20:15:48 -0300

    Configurando o settings  com utilizadção environ.get

---
+++ render_deploy/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RenderDeployConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'render_deploy'
+    verbose_name = 'Perguntas'
